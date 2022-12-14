ba28181ca3006a666d6370ea7053c859a737cbd3
Ciprian Ilut Pop <ciprian.ilut@ctb.upm.es>
2022-12-14 14:32:34 +0100

    first commit

--- bacnetweb/static/Database/schedual.sql
+++ bacnetweb/static/Database/schedual.sql
@@ -25,30 +25,3 @@ CREATE TABLE dailySchedules(
 );
 
 
-class Schedule(UserMixin, db.Model):
-    __tablename__ = "schedules"
-    id = db.Column(db.Integer, primary_key=True)
-    object_identifier = db.Column(db.String, nullable=False)
-    object_name = db.Column(db.String, nullable=False)
-    object_type = db.Column(db.String, nullable=False)
-    present_value = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    effective_period = db.Column(db.String, nullable=False)
-    exception_schedule = db.Column(db.String, nullable=False)
-    list_of_object_property_references = db.Column(db.String, nullable=False)
-    priority_for_writing = db.Column(db.String, nullable=False)
-    status_flags = db.Column(db.String, nullable=False)
-    reliability = db.Column(db.String, nullable=False)
-    out_of_service = db.Column(db.Boolean, nullable=False)
-    id_user = db.Column(db.Integer, nullable=False)
-
-
-
-class DailySchedule(UserMixin, db.Model):
-    __tablename__ = "dailySchedules"
-    id = db.Column(db.Integer, primary_key=True)
-    day = db.Column(db.String, nullable=False)
-    initial_time = db.Column(db.String, nullable=False)
-    final_time = db.Column(db.String, nullable=False)
-    value = db.Column(db.String, nullable=False)
-    object_id = db.Column(db.Integer, nullable=False)
